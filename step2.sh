50d295aa01d4bc44c27bb0fba1075d80155150e2
Josh Gubler <joshgubler@Joshs-MacBook-Pro.local>
2018-09-22 21:29:31 -0600

    send whole code

--- step2.sh
+++ step2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 subroutines/get_all_codes.sh
 next_code=`subroutines/find_next_code.sh`
-subroutines/submit_code.sh $next_code
+subroutines/submit_code.sh "$next_code"
 subroutines/is_error.sh
 subroutines/is_processing.sh
