bf5620c1d1ad99fa3790eb392ba78b5ba8618ed0
Josh Gubler <josh.gubler@gmail.com>
2018-09-22 21:41:04 -0600

    mock

--- subroutines/get_code.sh
+++ subroutines/get_code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p codes
-curl -s https://www.saintcon.org/pcsilo/code.php | grep '</text>' | tail -1 | cut -d '>' -f 2 | cut -d '<' -f 1 > codes/`hostname`
-#curl -s http://joshgubler-pc3-mock.s3-website-us-west-2.amazonaws.com/code.txt | grep '</text>' | tail -1 | cut -d '>' -f 2 | cut -d '<' -f 1 > codes/`hostname`
+#curl -s https://www.saintcon.org/pcsilo/code.php | grep '</text>' | tail -1 | cut -d '>' -f 2 | cut -d '<' -f 1 > codes/`hostname`
+curl -s http://joshgubler-pc3-mock.s3-website-us-west-2.amazonaws.com/code.txt | grep '</text>' | tail -1 | cut -d '>' -f 2 | cut -d '<' -f 1 > codes/`hostname`
--- subroutines/submit_code.sh
+++ subroutines/submit_code.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -s -F "command=$1" -F 'first=Enter ⏎' https://www.saintcon.org/pcsilo/index.php > response.txt
+#curl -s -F "command=$1" -F 'first=Enter ⏎' https://www.saintcon.org/pcsilo/index.php > response.txt
+echo $1
+curl -s http://joshgubler-pc3-mock.s3-website-us-west-2.amazonaws.com/processing.txt > response.txt
